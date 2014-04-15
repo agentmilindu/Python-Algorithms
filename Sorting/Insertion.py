f7e057334b7ea643f8ea9ba9f6dcafc1e556fa12
agentmilindu <agentmilindu@gmail.com>
2014-04-15 13:45:51 +0530

    Added Insertion Sort

---
+++ Sorting/Insertion.py
@@ -0,0 +1,16 @@
+def insertion_sort( list_to_sort ):
+    n = len(list_to_sort)
+    i = 1
+    while i < n:
+        number = list_to_sort[i]
+        j = i
+        while j >= 0:
+            if list_to_sort[j]>number:
+                list_to_sort[j+1]=list_to_sort[j]
+                j=j-1
+            else:
+                list_to_sort[j]=number
+                break
+            
+        
+        i += 1
